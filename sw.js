1d1ffdf6cc5c5a62682e070d7abca34c601f520c
backupdivinissima-byte <backupdivinissima@gmail.com>
2026-07-24 18:22:38 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(k => Promise.all(k.map(n => caches.delete(n))))
+      .then(() => self.registration.unregister())
+  );
+});
